d8e97ff9dd77f4d0263775fd40ce07788a5e7733
Michael Cannon <michael@typo3vagabond.com>
2013-06-22 16:53:39 -0700

    Text replacement, file mv, preps

--- prepare-plugin.sh
+++ prepare-plugin.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-OLD_TITLE="Custom Bulk/Quick Edit"
+NEW_CLASS="WordPress_Starter"
+NEW_FILTER="wordpress_starter_plugin"
+NEW_KB_PATH="20102742-WordPress-Starter-Plugin"
+NEW_SLUG="wordpress-starter-plugin"
 NEW_TITLE="WordPress Starter"
 
 OLD_CLASS="Custom_Bulk_Quick_Edit"
-NEW_CLASS="WordPress_Starter"
-
-OLD_SLUG="custom-bulk-quick-edit"
-NEW_SLUG="wordpress-starter-plugin"
-
 OLD_FILTER="custom_bulk_quick_edit"
-NEW_FILTER="wordpress_starter_plugin"
-
 OLD_KB_PATH="20112546-Custom-Bulk-Quick-Edit"
-NEW_KB_PATH="20102742-WordPress-Starter-Plugin"
+OLD_SLUG="custom-bulk-quick-edit"
+OLD_TITLE="Custom Bulk/Quick Edit"
 
-EXT_RENAME="css,php,pot"
+echo
+echo "Begin converting ${OLD_TITLE} to ${NEW_TITLE} plugin"
 
-find . -type f \( -name "*.md" -o -name "*.php" -o -name "*.txt" -o -name "*.xml" \)
+FILES=`find . -type f \( -name "*.md" -o -name "*.php" -o -name "*.txt" -o -name "*.xml" \)`
+for FILE in ${FILES} 
+do
+	perl -pi -e "s#${OLD_CLASS}#${NEW_CLASS}#g" ${FILE}
+	perl -pi -e "s#${OLD_FILTER}#${NEW_FILTER}#g" ${FILE}
+	perl -pi -e "s#${OLD_KB_PATH}#${NEW_KB_PATH}#g" ${FILE}
+	perl -pi -e "s#${OLD_SLUG}#${NEW_SLUG}#g" ${FILE}
+	perl -pi -e "s#${OLD_TITLE}#${NEW_TITLE}#g" ${FILE}
+done
+
+mv ${OLD_SLUG}.css ${NEW_SLUG}.css
+mv ${OLD_SLUG}.php ${NEW_SLUG}.php
+mv languages/${OLD_SLUG}.pot languages/${NEW_SLUG}.pot
+mv lib/class-${OLD_SLUG}-settings.php lib/class-${NEW_SLUG}-settings.php

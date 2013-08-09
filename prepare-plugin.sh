fc2722ff96aa7613efd7e410e1369c4ec4a5d6fe
Michael Cannon <mc@aihr.us>
2013-08-09 17:51:54 +0800

    Allow blank NEW_* settings

--- prepare-plugin.sh
+++ prepare-plugin.sh
@@ -22,16 +22,39 @@ echo "Begin converting ${OLD_TITLE} to ${NEW_TITLE} plugin"
 FILES=`find . -type f \( -name "*.md" -o -name "*.php" -o -name "*.txt" -o -name "*.xml" \)`
 for FILE in ${FILES} 
 do
-	perl -pi -e "s#${OLD_ABBR}#${NEW_ABBR}#g" ${FILE}
-	perl -pi -e "s#${OLD_CLASS}#${NEW_CLASS}#g" ${FILE}
-	perl -pi -e "s#${OLD_FILTER}#${NEW_FILTER}#g" ${FILE}
-	perl -pi -e "s#${OLD_KB_PATH}#${NEW_KB_PATH}#g" ${FILE}
-	perl -pi -e "s#${OLD_SLUG}#${NEW_SLUG}#g" ${FILE}
-	perl -pi -e "s#${OLD_TITLE}#${NEW_TITLE}#g" ${FILE}
+	if [[ '' != ${NEW_ABBR} ]]
+	then
+		perl -pi -e "s#${OLD_ABBR}#${NEW_ABBR}#g" ${FILE}
+	fi
+
+	if [[ '' != ${NEW_CLASS} ]]
+	then
+		perl -pi -e "s#${OLD_CLASS}#${NEW_CLASS}#g" ${FILE}
+	fi
+
+	if [[ '' != ${NEW_FILTER} ]]
+	then
+		perl -pi -e "s#${OLD_FILTER}#${NEW_FILTER}#g" ${FILE}
+	fi
+
+	if [[ '' != ${NEW_KB_PATH} ]]
+	then
+		perl -pi -e "s#${OLD_KB_PATH}#${NEW_KB_PATH}#g" ${FILE}
+	fi
 
 	if [[ '' != ${NEW_SITE} ]]
 	then
 		perl -pi -e "s#${OLD_SITE}#${NEW_SITE}#g" ${FILE}
+	fi
+
+	if [[ '' != ${NEW_SLUG} ]]
+	then
+		perl -pi -e "s#${OLD_SLUG}#${NEW_SLUG}#g" ${FILE}
+	fi
+
+	if [[ '' != ${NEW_TITLE} ]]
+	then
+		perl -pi -e "s#${OLD_TITLE}#${NEW_TITLE}#g" ${FILE}
 	fi
 done
 

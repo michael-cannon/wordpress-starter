355488aaef32a566f3af74657322887f73f01f27
Michael Cannon <mc@aihr.us>
2013-11-29 15:01:03 -0800

    Coding standard

--- wordpress-starter.php
+++ wordpress-starter.php
@@ -477,7 +477,7 @@ class WordPress_Starter extends Aihrus_Common {
 	public static function notice_0_0_1() {
 		$text = sprintf( __( 'If your WordPress Starter display has gone to funky town, please <a href="%s">read the FAQ</a> about possible CSS fixes.', 'wordpress-starter' ), 'https://aihrus.zendesk.com/entries/23722573-Major-Changes-Since-2-10-0' );
 
-		parent::notice_updated( $text );
+		self::notice_updated( $text );
 	}
 
 
